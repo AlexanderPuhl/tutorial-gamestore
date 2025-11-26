e78ced469090f35b52f1905fd79412f9788f8495
Alex Puhl <alexpuhldeveloper@gmail.com>
2025-11-25 23:35:56 -0700

    Finished video 10

---
+++ plugins/blocks-gamestore/src/block-header/save.js
@@ -0,0 +1,27 @@
+import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
+
+export default function save({ attributes }) {
+  const { memberLink, cartLink } = attributes;
+
+  return (
+    <div {...useBlockProps.save()}>
+      <div className="inner-header">
+        <InnerBlocks.Content />
+        <div className="right-section">
+          <div className="header-search"></div>
+          <div className="header-mode-switcher"></div>
+          {cartLink && (
+            <div className="header-cart-link">
+              <a href={cartLink}></a>
+            </div>
+          )}
+          {memberLink && (
+            <div className="header-member-link">
+              <a href={memberLink}>Member Area</a>
+            </div>
+          )}
+        </div>{' '}
+      </div>
+    </div>
+  );
+}
